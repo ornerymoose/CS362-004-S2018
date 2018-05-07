fafac070fd68b9040bff8d6105ced3a73a55019c
ornerymoose <dpardy87@gmail.com>
2018-05-06 22:49:11 -0600

    pushing testme files and Makefile to pardyd-random-quiz branch

---
+++ projects/pardyd/quiz/testme.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar(){
+  char c = rand() % 94 + 32;
+  return c;
+}
+
+char *inputString(){
+    const int SIZE = 6; //set 
+    char c;
+    static char array[SIZE];
+
+    int i = 0;
+    do {
+      c = ((rand() % 16) + 101);
+      array[i] = c;
+      i++;
+    } while (i < 5);
+
+    array[5] = '\0'; //null terminator
+    return array; //return array of characters, which is effectively a string
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
